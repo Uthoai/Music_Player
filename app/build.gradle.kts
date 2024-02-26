6d8863b8a87066e24806bc6579ab1bba43374c45
Uthoai Marma <uthoaimarma597@gmail.com>
2024-02-26 14:15:54 +0600

    add playlistsActivity,favouriteActivity, playActivity UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,5 @@ dependencies {
     implementation("com.intuit.sdp:sdp-android:1.1.0")
     implementation("com.intuit.ssp:ssp-android:1.1.0")
 
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 }
---
+++ app/src/main/java/com/best/free/top/music/player/view/FavouritesActivity.kt
@@ -0,0 +1,12 @@
+package com.best.free.top.music.player.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.best.free.top.music.player.R
+
+class FavouritesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favourites)
+    }
+}
